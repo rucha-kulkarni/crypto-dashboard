37e7265ccddd1798b4e82b2c78b4793292209daa
rucha-kulkarni <rucha3702@gmail.com>
2023-12-29 23:07:20 +0530

    project submit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Coin from "./pages/Coin";
+import Compare from "./pages/Compare";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Watchlist from "./pages/Watchlist";
+// import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+
+function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9",
+      },
+    },
+  });
+
+  var cursor;
+  var cursorPointer;
+
+
+
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/coin/:id" element={<Coin />} />
+            <Route path="/compare" element={<Compare />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
